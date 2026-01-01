de32e5960aa5481f8c36cd63a810a08768ef542d
rizwan-4308 <rizwan4308@gmail.com>
2026-01-01 13:58:04 +0530

    Type Casting

---
+++ P02_Variables/P03_Type_Casting/type casting.py
@@ -0,0 +1,28 @@
+name = "Md Rizwan"
+age = 25
+gpa = 3.2
+is_student = True
+
+#print(type(name))
+#print(type(age))
+#print(type(gpa))
+#print(type(is_student))
+
+gpa = int(gpa)
+print(gpa)      #3
+
+age = float(age)
+print(age)      #25.0
+
+age = str(age)
+print(age)      #25.0
+print(type(age))        #str
+#age +=1
+age += "1"
+
+print(age)
+
+#
+name = bool(name)
+
+print(name)
