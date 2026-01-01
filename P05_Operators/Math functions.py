ab3c8a2363284ac32ad352784045cf4a3bdd01c5
rizwan-4308 <rizwan4308@gmail.com>
2026-01-01 15:08:20 +0530

    Matn functions

---
+++ P05_Operators/Math functions.py
@@ -0,0 +1,22 @@
+import math
+
+x = 3.14
+y = 4
+z = 9.4
+
+# result = round(x)
+# result = abs(y)
+# result = pow(y, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+
+# print(result)
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(z)
+# result = math.ceil(z)
+result = math.floor(9.8)
+
+print(result)
