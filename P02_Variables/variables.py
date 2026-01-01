36f739d36c24b01ec69ad40479bdbb9a3e5d3497
rizwan-4308 <rizwan4308@gmail.com>
2026-01-01 13:43:14 +0530

    Variables

---
+++ P02_Variables/variables.py
@@ -0,0 +1,49 @@
+# Strings
+first_name = "Rizwan"
+food = "pizza"
+email = "rizwan123@email.com"
+
+print(first_name)
+print(f"Hello {first_name}!")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old.")
+print(f"you are buying {quantity} {food}/s")
+print(f"Your class has {num_of_students} students.")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}Kms")
+
+# Boolean
+is_student = True
+for_sale = True
+is_online = False
+
+#print(f"Are you a student?: {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+##//
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is not available")
+
+## //
+if is_online:
+    print("You are a online")
+else:
+    print("You are offline")
