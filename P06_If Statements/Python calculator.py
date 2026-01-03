827a6efe0c7f5d5d33f876cacdd65fbe0034f495
rizwan-4308 <rizwan4308@gmail.com>
2026-01-03 12:12:25 +0530

    Python Calculator

---
+++ P06_If Statements/Python calculator.py
@@ -0,0 +1,18 @@
+# python calculator
+
+operator = input("Enter an operator (+ - * /):")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd operator:"))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
