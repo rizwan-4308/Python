971cd21bbe4a18cbac8fc08aff4c8287daa62af0
rizwan-4308 <rizwan4308@gmail.com>
2026-01-08 15:23:30 +0530

    Conditional Operator / Ternary Operator

---
+++ P08_Conditional_Expression/Conditional Expression.py
@@ -0,0 +1,20 @@
+
+num = 6
+a = 6
+b = 7
+age = 25
+temperature = 37
+user_role = "guest"
+
+# print("Positive" if num > 0 else "negative")
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else"Child"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+
+# print(max_num)
+# print(min_num)
+# print(status)
+print(access_level)
