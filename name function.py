5a2b01678e8598572701f458facebf1cd7e489a1
rizwan-4308 <rizwan4308@gmail.com>
2026-01-09 11:52:18 +0530

    Ex1_name function

---
+++ name function.py
@@ -0,0 +1,25 @@
+
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone #: ")
+
+# result = len(name)
+# result  = name.find("R")
+# result = name.rfind("a")
+# result = name.find("Q")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+
+
+# print(result)
+# print(name)
+# print(result)
+
+# result = phone_number.count("-")
+# phone_number = phone_number.replace("-", " ")
+phone_number = phone_number.replace("-", "")
+
+# print(result)
+print(phone_number)
