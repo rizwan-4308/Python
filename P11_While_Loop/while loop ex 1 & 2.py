c884b523f61ae27216c0104d9bbaa6449863dc60
rizwan-4308 <rizwan4308@gmail.com>
2026-01-13 12:40:25 +0530

    While loops examples

---
+++ P11_While_Loop/while loop ex 1 & 2.py
@@ -0,0 +1,18 @@
+
+name = input("Enter your name: ")
+
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name: ")
+# else:
+#     print(f"Hello {name}")
+
+###
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old.")
