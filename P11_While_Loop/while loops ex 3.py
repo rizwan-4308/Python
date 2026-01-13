027b9796f0e564f1e5b02399f6ca50bd342a835e
rizwan-4308 <rizwan4308@gmail.com>
2026-01-13 12:45:38 +0530

    While loops ex 3

---
+++ P11_While_Loop/while loops ex 3.py
@@ -0,0 +1,12 @@
+### example 3
+
+food = input("Enter your favorite food (q to quit): ")
+
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter your another favorite food (q to quit): ")
+
+print("bye")
+
+
+
