57b2dd3541f62edb52e6fd52ae142e58c52d51b3
rizwan-4308 <rizwan4308@gmail.com>
2026-01-13 13:59:03 +0530

    For loops

---
+++ P13_For_Loops/for loops.py
@@ -0,0 +1,18 @@
+
+# for x in reversed(range(1, 11, 2)):
+#     print(x)
+
+"""
+for x in range(1, 21):
+    if x == 13:
+        continue
+    else:
+        print(x)
+"""
+# to stop at a particular digit
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
