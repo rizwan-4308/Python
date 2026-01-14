1947bfa405f84fc4df427078c0c8985ebdd60d07
rizwan-4308 <rizwan4308@gmail.com>
2026-01-14 12:19:06 +0530

    Collection_Tuple

---
+++ P16_Collection_List_Set_Tuple/Collection_Tuple.py
@@ -0,0 +1,16 @@
+
+fruits = ("apple", "orange", "banana", "coconut", "pineapple", "banana")
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+# print("lemon" in fruits)
+
+# print(fruits.index("apple"))
+# print(fruits.index("coconut"))
+# print(fruits.count("banana"))
+
+# print(fruits)
+for fruit in fruits:
+    print(fruit)
