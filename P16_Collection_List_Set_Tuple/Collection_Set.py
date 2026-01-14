9f51cee1aed64039427805977e9a8d68f7526502
rizwan-4308 <rizwan4308@gmail.com>
2026-01-14 12:12:54 +0530

    Collection_Sets

---
+++ P16_Collection_List_Set_Tuple/Collection_Set.py
@@ -0,0 +1,16 @@
+
+fruits = {"apple", "orange", "banana", "coconut", "pineapple", "banana"}
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+# print("lemon" in fruits)
+
+# print(fruits[0])
+
+# fruits.add("lemon")
+# fruits.remove("banana")
+# fruits.pop()
+fruits.clear()
+
+print(fruits)
