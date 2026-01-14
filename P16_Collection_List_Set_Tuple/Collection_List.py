62707ca729f4c3d48e717d097ccddb287a3e27b5
rizwan-4308 <rizwan4308@gmail.com>
2026-01-14 12:04:44 +0530

    Collection_List

---
+++ P16_Collection_List_Set_Tuple/Collection_List.py
@@ -0,0 +1,89 @@
+## List
+
+fruits = ["apple", "orange", "banana", "coconut", "pineapple", "banana"]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+# print("lemon" in fruits)
+
+
+
+# print(fruits)
+# print(fruits[0])
+# print(fruits[2])
+
+# print(fruits[0:3])
+# print(fruits[::2])
+# print(fruits[::-1])
+
+
+# for x in fruits:
+#     print(x)
+
+# for fruit in fruits:
+#    print(fruit)
+
+"""
+fruits[0] = "pineapple"
+for fruit in fruits:
+   print(fruit)
+"""
+
+""" # to add an element in a list
+fruits.append("lemon")
+
+print(fruits)
+"""
+
+"""
+# to add an element in a list
+fruits.remove("banana")
+print(fruits)
+"""
+
+"""
+# To add an element at any index
+fruits.insert(0, "banana")
+print(fruits)
+"""
+
+"""
+# to sort the list
+fruits.sort()
+print(fruits)
+"""
+
+"""
+# to reverse a list by the order we placed 
+fruits.reverse()
+print(fruits)
+"""
+
+"""
+# to reverse a list by the alphabetical order
+fruits.reverse()
+print(fruits)
+"""
+
+"""
+# to clear a list
+fruits.clear()
+print(fruits)
+"""
+
+"""
+print(fruits.index("apple"))
+print(fruits.index("coconut"))
+# print(fruits.index("lemon"))
+"""
+
+
+print(fruits.count("banana"))
+print(fruits.count("lemon"))
+print(fruits)
+
+
+
+
+
