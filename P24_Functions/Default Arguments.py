63114b5f039ff2ca670b710b55638a12982f4ad1
rizwan-4308 <rizwan4308@gmail.com>
2026-01-15 14:39:21 +0530

    Default agruments

---
+++ P24_Functions/Default Arguments.py
@@ -0,0 +1,30 @@
+import time
+
+"""
+def net_price(list_price, discount, tax):
+    return list_price * (1 - discount) * (1 + tax)
+
+print((net_price(500, 0, 0.05)))
+"""
+
+
+""" 
+# to set discount and tax same
+def net_price(list_price, discount=0, tax=0.05):
+    return list_price * (1 - discount) * (1 + tax)
+
+# print((net_price(500)))
+# print((net_price(500, 0.1)))
+print((net_price(500, 0.1, 0)))
+"""
+
+# exercise 1
+
+
+def count(end, start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("DONE!")
+
+count(30, 15)
