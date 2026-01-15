3da5c6ce132cd605bc9a9772e5a7772a870e5872
rizwan-4308 <rizwan4308@gmail.com>
2026-01-15 13:33:05 +0530

    Functins ex1

---
+++ P24_Functions/Functions ex_1.py
@@ -0,0 +1,29 @@
+
+"""
+def happy_birthday(name, age):
+    print(f"Happy Birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy Birthday to you!")
+    print()
+"""
+
+# happy_birthday()
+# happy_birthday()
+
+"""
+def happy_birthday(age, name):
+    print(f"Happy Birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy Birthday to you!")
+    print()
+"""
+
+def happy_birthday(x, y):
+    print(f"Happy Birthday to {x}!")
+    print(f"You are {y} years old!")
+    print("Happy Birthday to you!")
+    print()
+
+happy_birthday("alex", 20)
+happy_birthday("steve", 30)
+happy_birthday("john", 40)
