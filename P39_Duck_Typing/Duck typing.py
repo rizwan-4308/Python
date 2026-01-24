18970d01ca16f43715db5b0e4276acfae093fd58
rizwan-4308 <rizwan4308@gmail.com>
2026-01-24 11:48:32 +0530

    Duck Typing

---
+++ P39_Duck_Typing/Duck typing.py
@@ -0,0 +1,25 @@
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Car:
+
+    alive =  False
+
+    # def horn(self):
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
