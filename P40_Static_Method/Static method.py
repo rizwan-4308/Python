fb2c1104e1c64dc9f07a0adf4b661ddf34d7e3e0
rizwan-4308 <rizwan4308@gmail.com>
2026-01-24 12:06:02 +0530

    Static method

---
+++ P40_Static_Method/Static method.py
@@ -0,0 +1,27 @@
+
+class Employee:
+
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "janitor"]
+        return position in valid_positions
+
+# Static method
+employee1 = Employee("Eugene", "Manager")
+employee2 = Employee("Squidward", "Cashier")
+employee3 = Employee("Spongebob", "Cook")
+
+print(Employee.is_valid_position("Manager"))
+
+print("")
+# Instance method
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())
